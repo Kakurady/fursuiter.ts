9ea372e5b15f6b7b2495f67223099acf175a7d1a
Kakurady <kakurady@gmail.com>
2018-09-03 21:14:25 -0400

    port file loading.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Todo
 =====
 - split files into browser-size & node-side
 - enforce checks for undefined before use (src/lib/gen_pp3.ts:gen_title)
+- figure out if repeating name of event in tags makes sense
+- `loadSpecies()` can return a `Species` or a `string`. `gen_pp3` is fine either way, but maybe this could be tightened.
---
+++ src/lib/FileSystemDataSource.ts
@@ -0,0 +1,110 @@
+import { Event, Character, Performer, Species, Maker } from "./types";
+
+import { readFile } from "fs";
+import { promisify } from "util";
+
+const readFileAsync = promisify(readFile);
+
+export default class FileSystemDataSource {
+    dataPath: string;
+
+    /**
+     *
+     * @param dataPath
+     */
+    constructor(dataPath: string) {
+        this.dataPath = dataPath;
+    }
+
+
+    async loadCharacter(name: string): Promise<Character | null> {
+        try {
+            const s = await readFileAsync(`${this.dataPath}/fursuit/${name}.json`, {
+                encoding: "utf8"
+            });
+            const obj = JSON.parse(s);
+            return obj;
+        } catch (e) {
+            if (e.code == "ENOENT") {
+                return null;
+            } else {
+                throw e;
+            }
+        }
+    }
+
+    async loadPerformer(name: string): Promise<Performer | null> {
+        try {
+            const s = await readFileAsync(`${this.dataPath}/performer/${name}.json`, {
+                encoding: "utf8"
+            });
+            const obj = JSON.parse(s);
+            return obj;
+        } catch (e) {
+            if (e.code == "ENOENT") {
+                return null;
+            } else {
+                throw e;
+            }
+        }
+    }
+
+    // FIXME can name input be trusted here?
+    async loadMaker(name: string): Promise<Maker | string> {
+        try {
+            // most makers don't have data, so just return as-is later,
+            // but if data is later added it won't be found, so change to lowercase.
+            //
+            // hopefully I won't ever need toLocaleLowerCase() here.
+            const filename = name.toLowerCase().replace(/ /g, "_");
+
+            const s = await readFileAsync(`${this.dataPath}/maker/${filename}.json`, { 'encoding': 'utf8' });
+
+            const obj = JSON.parse(s);
+            return obj;
+        } catch (e) {
+            if (e.code == "ENOENT") {
+                return name;
+            } else {
+                throw e;
+            }
+        }
+    }
+
+    async loadSpecies(name: string): Promise<Species | string> {
+        try {
+            const s = await readFileAsync(`${this.dataPath}/species/${name}.json`, {
+                encoding: "utf8"
+            });
+
+            const obj = JSON.parse(s);
+            return obj;
+        } catch (e) {
+            if (e.code == "ENOENT") {
+                return name;
+            } else {
+                throw e;
+            }
+        }
+    }
+
+    async loadEvent(name: string): Promise<Event> {
+        try {
+            const s = await readFileAsync(`${this.dataPath}/event/${name}.json`, {
+                encoding: "utf8"
+            });
+            const obj = JSON.parse(s);
+            // type checking is not enforced here currently
+            return obj;
+        } catch (e) {
+            if (e.code == "ENOENT") {
+                // return a default event.
+                // FIXME: most of the event files don't repeat the names in the tags - which type is appropriate?
+                // also, this should probably be in a different area
+                return { name, tags: [name] };
+            } else {
+                throw e;
+            }
+        }
+    }
+}
