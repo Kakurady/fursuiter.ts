41c90b2bb2566623835c806c554cc06929f0fd5d
Kakurady <kakurady@gmail.com>
2023-12-05 20:15:20 -0500

    Move introductory text out of init()

--- src/lib/mkpp3_core.ts
+++ src/lib/mkpp3_core.ts
@@ -482,13 +482,7 @@ async function init() {
 
         // TODO: handle errors
         ds.watchChanges((eventType, filename) => { if (eventType === "rename") { updateAllEntries(); } });
-        // short names for exported items
-        console.log("Frequently used objects and functions: \n");
-        console.log("Character & performer list (live updates): fursuits (f), performers (p)");
-        console.log("(To inspect character/performer, use ds.loadCharacter or ds.loadPerformer)");
-        console.log("Write profile to file system: await convAndWrite() ( await w() )");
-        console.log("Maintenance: findAllSpeciesAndTags");
-        // console.log("Debug: ds");
+
         return {
             ...context,
             characters,
--- src/mkpp3.ts
+++ src/mkpp3.ts
@@ -4,6 +4,13 @@ import { start } from "repl";
 async function main(){
     try {
         const f = await init();
+        // short names for exported items
+        console.log("Frequently used objects and functions: \n");
+        console.log("Character & performer list (live updates): fursuits (f), performers (p)");
+        console.log("(To inspect character/performer, use ds.loadCharacter or ds.loadPerformer)");
+        console.log("Write profile to file system: await convAndWrite() ( await w() )");
+        console.log("Maintenance: findAllSpeciesAndTags");
+        // console.log("Debug: ds");
         const r = start({prompt: ">> ", useGlobal: false});
         r.context.m = "msg";
         Object.assign(r.context, f);
