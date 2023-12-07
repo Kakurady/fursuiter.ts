19630464dbd8e9faa8686a44bba82c2dbef7c2b1
Kakurady <kakurady@gmail.com>
2023-12-07 07:40:52 -0500

    Add comments
    The best code comments should not only explain what the code does or how it
    achieves its goal, but give context on why the code does what it does.
    
    Because while the former can be deduced by analyzing the code, admit it,
    nobody is going to remember the latter when coming back to a code base
    after a few years. This is even worse for hobby projects.

--- src/import.ts
+++ src/import.ts
@@ -14,9 +14,12 @@ async function importFromConsole(){
     let result = response.split("\t").map(x => x.trim()).map(x=>0 == x.length? undefined: x);
     let [timestamp, performer, character, species_s, maker_s, web, fa, instagram, telegram, tiktok, twitter, site1, link1, site2, link2, site3, link3, discord] = result;
 
+    // because Gboard will capitalize the first letter in each input field, including common species such as "Fox", undo that here.
+    // this will lower-case proper nouns such as "Pokémon" or "Neopet" as an unwanted effect, but let's fix that by hand afterwards.
     let species = species_s && species_s.split("\t").map(x => x.trim().toLowerCase()) || [];
     let maker = maker_s && maker_s.split("\t").map(x => x.trim()) || [];
     let characterRecord: CharacterRecord = { name: character, performer, maker, species };
+    // convert write-in entries
     let [on1, on2, on3] = [[site1, link1], [site2, link2], [site3, link3]].map(x => {
         let [site, link] = x;
         if (!site) { return; }
@@ -70,6 +73,7 @@ async function importFromConsole(){
             delete on[key];
         }
     }
+    // transfer performer name to character name field, if character name is blank
     if (performer && !character)
     {
         characterRecord.name = character;
@@ -81,14 +85,19 @@ async function importFromConsole(){
 
     function getCandidateFilename()
     {
+        // strip accents: https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
+        // this is not strictly accurate (for example, it might make more sense to convert Ü into UE), but close enough for our purposes
         let name_words = characterRecord.name.toLowerCase().normalize("NFKD").replace(/(\p{Diacritic}|[\-'&\?/])/gu, "").split(" ");
+        // check how "common" the name is using a password strength checker
         let entropy = zxcvbn(name_words[0]).guesses_log10;
         if (entropy >= 5)
         {
+            // the name is pretty uncommon (one in 100,000), use it as-is
             return name_words[0];
         }
         if (characterRecord.species[0] && typeof characterRecord.species[0] == "string")
         {
+            // a species is available, use `name_species`
             let species = characterRecord.species[0].toLowerCase().normalize("NFKD").replace(/(\p{Diacritic}|[\-'&\?/])/gu, "").replace(/ /g, "_");
             return `${name_words[0]}_${species}`;
         }
