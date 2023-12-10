2785f94c1c9275aaf37d238058577a21dcc523e3
Kakurady <kakurady@gmail.com>
2023-12-10 07:25:58 -0500

    Save character record and merge from existing on storage

--- src/import.ts
+++ src/import.ts
@@ -1,12 +1,13 @@
 import { init } from "./lib/mkpp3_core";
 import {stdin, stdout, exit} from "process";
-import { CharacterRecord, SiteName } from "./lib/types";
+import { CharacterRecord, DataSource, SiteName } from "./lib/types";
+import {red, white, bold} from "kleur";
 
 const prompts = require("prompts");
 const zxcvbn = require("zxcvbn");
 const readline = require("node:readline/promises");
 
-async function importFromConsole(){
+async function importFromConsole(ds: DataSource) {
     // const response = await prompts({ type: 'text', name: 'value', message: 'Paste row from response spreadsheet:' });
     const rl = readline.createInterface({ input: stdin, output: stdout });
     const response: string = await rl.question("Paste row from response spreadsheet:\n");
@@ -18,7 +19,6 @@ async function importFromConsole(){
     // this will lower-case proper nouns such as "Pokémon" or "Neopet" as an unwanted effect, but let's fix that by hand afterwards.
     let species = species_s && species_s.split("\t").map(x => x.trim().toLowerCase()) || [];
     let maker = maker_s && maker_s.split("\t").map(x => x.trim()) || [];
-    let characterRecord: CharacterRecord = { name: character, performer, maker, species };
     // convert write-in entries
     let [on1, on2, on3] = [[site1, link1], [site2, link2], [site3, link3]].map(x => {
         let [site, link] = x;
@@ -33,6 +33,8 @@ async function importFromConsole(){
                 ret["twitter"] = link; return ret;
             case "second life":
                 ret["second_life"] = link; return ret;
+            case "furry network":
+                ret["furrynetwork"] = link; return ret;
             case "bsky":
             case "cohost":
             case "da":
@@ -63,7 +65,7 @@ async function importFromConsole(){
                 ret[lowerSite] = link;
                 return ret;
             default:
-                console.warn(`not sure what to do with ${site}`);
+                console.warn(`${red("not sure what to do with")} ${white(bold(site))}`);
                 return ret;
         }
     });
@@ -73,18 +75,22 @@ async function importFromConsole(){
             delete on[key];
         }
     }
-    // transfer performer name to character name field, if character name is blank
-    if (performer && !character)
-    {
-        characterRecord.name = character;
-        delete characterRecord.performer;
-    }
 
-    characterRecord.on = on;
-    console.log(characterRecord);
+    // transfer performer name to character name field, if character name is blank
+    if (performer && !character) {
+        character = performer;
+        performer = undefined;
+    }
+    else if (performer == character) {
+        performer = undefined;
+    }
+    let characterRecord: CharacterRecord = { name: character, performer, maker, species };
+
+    console.log({...characterRecord, on});
 
     function getCandidateFilename()
     {
+        if (!characterRecord.name) {return;}
         // strip accents: https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
         // this is not strictly accurate (for example, it might make more sense to convert Ü into UE), but close enough for our purposes
         let name_words = characterRecord.name.toLowerCase().normalize("NFKD").replace(/(\p{Diacritic}|[\-'&\?/])/gu, "").split(" ");
@@ -104,8 +110,35 @@ async function importFromConsole(){
         return name_words.join("_");
     }
     let candidateFilename = getCandidateFilename();
-    let filename = await prompts([{name: "filename", type: "text", message: "Enter filename for character record: ", initial: candidateFilename}]);
+    // surprising behavior from prompts: enter takes default, but any key removes it. but tab allows you to edit it. almost as if the default is tab autocomplete, and the prompt starts blank. blank is default for many CLI so it makes sense, but usually the default value would be in parenthesis. autocomplete candidate should be lowlighted, which is not done here.
+    let filenameResponse = await prompts([{ name: "filename", type: "text", message: "Enter filename for character record: ", initial: candidateFilename }]);
+    let filename: string = filenameResponse.filename;
     console.log(filename);
+    if (!filename) {
+        console.log(red(`${bold(filename)} is empty!\n`))
+        return;
+    }
+    let oldCharacter = await ds.loadCharacter(filename);
+    let newCharacter: CharacterRecord = {};
+    if (oldCharacter) {
+        let newOn = { ...on, ...oldCharacter.on };
+        let newSpecies = new Set([...species, ...oldCharacter.species]);
+        let newMaker = new Set([...maker, ...oldCharacter.maker]);
+        newCharacter = { name: character, performer, ...oldCharacter, on: { ...newOn }, maker: [...newMaker], species: [...newSpecies] };
+        console.log(bold("merge with existing data"));
+        console.log(newCharacter);
+        let resp = await prompts([{ name: "confirm", type: "confirm", message: "write to the file system?" }]);
+        if (resp.confirm) {
+            ds.saveCharacter(filename, newCharacter);
+            console.log(`${white(bold(filename))} saved!\n`)
+        }
+    }
+    else {
+        newCharacter = { ...characterRecord, on };
+        ds.saveCharacter(filename, newCharacter);
+        console.log(`${white(bold(filename))} saved!\n`)
+    }
+    // TODO: check if file exists and offer to merge if appropriate
 }
 
 async function main(){
@@ -114,7 +147,7 @@ async function main(){
         while (true)
         {
             try {
-                await importFromConsole();
+                await importFromConsole(f.ds);
             } catch (error) {
                 throw error;
             }
